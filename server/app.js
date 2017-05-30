447f515c98e6c5114ed1189209273edbb435a513
doxtu <nickwgonzales@gmail.com>
2017-05-30 18:06:25 -0500

    generic commit

--- server/app.js
+++ server/app.js
@@ -10,6 +10,6 @@ var app = express();
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname,"../public")));
 
 app.listen(port);
