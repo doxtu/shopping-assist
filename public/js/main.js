9dcc9792a8b10f14f8e73dbbde477319a0b8fc76
doxtu <nickwgonzales@gmail.com>
2017-06-03 23:01:07 -0500

    AJAX added

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,23 @@
 $(document).ready(function exe(){
-  console.log("hi");
+  $("#searchSubmit").on("click",function search(){
+    let search = $("#searchInput").val();
+    let url = "http://api.walmartlabs.com/v1/search?format=json&apiKey=nr8nmaedubfzep9kys6syxdy&callback=?&query=" + search;
+    $.getJSON(url,function handleSearch(results){
+      let items = results.items;
+      console.log(items);
+      items.forEach(function appender(item){
+        $("#predictorList").append(`
+          <h3 class="text-center">${item.name}</h3>
+          <img class="center-block" src=${item.thumbnailImage}>
+          <p class="text-center">Price: ${item.salePrice}</p>
+          `);
+      });
+    });
+  });
 });
+
+/*
+  When you search, there will be multiple results in the search.items array,
+  create a container with the results and display it with a select button
+  need names, thumbnailImage, msrp, salePrice
+*/
