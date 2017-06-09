fffcc034929cf562e7c6546db2d34d40c87f4774
Nick G <nickwgonzales@gmail.com>
2017-06-08 21:58:17 -0500

    Finished prototype interface. Added state

--- public/js/main.js
+++ public/js/main.js
@@ -1,23 +1,56 @@
 $(document).ready(function exe(){
-  $("#searchSubmit").on("click",function search(){
+  /*
+    State variables. Manage the current shopping list
+  */
+  let curlist = {};
+
+  (function init(){
+    let list = $("#resultsList li");
+    Array.prototype.forEach.call(list,(_,i)=>{
+      list[i].addEventListener("click",selectItem);
+    });
+  })();
+  /*
+    Ajax for items from Walmart API.
+  */
+  $("#searchSubmit, #searchInput").on("click keydown",function search(e){
+    if(this === document.querySelector("#searchSubmit")) e.key = "Enter";
+    if(typeof e.key === "undefined" || e.key !== "Enter") return;
     let search = $("#searchInput").val();
     let url = "http://api.walmartlabs.com/v1/search?format=json&apiKey=nr8nmaedubfzep9kys6syxdy&callback=?&query=" + search;
     $.getJSON(url,function handleSearch(results){
       let items = results.items;
-      console.log(items);
-      items.forEach(function appender(item){
-        $("#predictorList").append(`
-          <h3 class="text-center">${item.name}</h3>
-          <img class="center-block" src=${item.thumbnailImage}>
-          <p class="text-center">Price: ${item.salePrice}</p>
+      let list = $("#resultsList li");
+      items.forEach(function appender(item,i){
+        let name = item.name,
+        category = item.categoryPath.split("/")[0] || "General",
+        price = accounting.formatMoney(item.msrp || item.salePrice),
+        image = item.thumbnailImage;
+        list[i].innerHTML = (`
+          <img src=${image}>
+          <h3>${name}</h3>
+          <p>Category: ${category}</p>
+          <p>Price: ${price}</p>
           `);
       });
     });
   });
-});
 
-/*
-  When you search, there will be multiple results in the search.items array,
-  create a container with the results and display it with a select button
-  need names, thumbnailImage, msrp, salePrice
-*/
+  /*
+    Item selected event functions
+  */
+  function selectItem(e){
+    let name = this.children[1].innerHTML,
+    category = this.children[2].innerHTML.split(": ")[1],
+    price = +accounting.unformat(this.children[3].innerHTML.split(": ")[1]);
+
+    if(typeof curlist[category] === "undefined"){
+        curlist[category] = {};
+    }
+    curlist[category][name] = {
+      name:name,
+      price:price
+    };
+  }
+
+});
